ce81f9ec902ece62760bfb81fec62322f53c48f2
Jan <fog.borchersboy@gmail.com>
2016-10-23 22:28:18 +0200

    Add files via upload

---
+++ regeln.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML>
+
+
+<html>
+    <head>
+        <title>Fritz - Humpeee</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+
+
+    <body>
+
+        <header> 
+
+            <center><font size="25">Blackwater - RPG </font></center>
+
+
+            <ul>
+                <nav>
+
+                    <font size="5">
+
+                    <li><a href="index.php">Home</li></a>
+                    <li><a href="regeln.php">Regeln</li></a>
+                    <li><a href="sonstiges.php">Sonstiges</li></a>
+
+                    </font>
+
+                </nav>
+
+            </ul>
+
+
+            <center>============================================================</center>
+
+
+        </header>
+
+        <br>
+        <div class="datagrid"><table>
+                <thead><tr><th>Regel</th><th>Strafe</th><th>Bemerkung</th></tr></thead>
+                <tbody><tr><td>Nicht schreien</td><td>Schreien wird bestraft mit einem Bann von 24h!</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Nicht beleidigen</td><td>Beleidigen wird bestraft mit einem Bann!</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                </tbody>
+            </table>
+        </div>
+
+        <video id="video_background" preload="auto" autoplay="true" loop="loop" muted="muted" volume="0"> 
+            <source src="humpeee.webm" type="video/webm"> 
+            <source src="humpeee.mp4" type="video/mp4"> 
+            <source src="humpeee.ogv" type="video/ogg ogv">
+            Video not supported </video>
+
+        <div id="video_pattern"></div>
+
+        <footer>
+        </footer>
+
+    </body>
+
+
+
+
+
+</html>
